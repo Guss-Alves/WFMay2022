adadbca0e1ab1e57a39a8b11322f32b54cd5316e
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2022-05-22 13:24:51 -0700

    fixed

---
+++ CSS-WORK/js-work/loop-challenges/loop.js
@@ -0,0 +1,15 @@
+// for( var i = 1; i < 21; i++){
+//     if( i % 2 == 1){
+//         console.log(i)
+//     }
+// }
+
+// for( var i = 100; i > 0; i--){
+//     if( i % 3 == 0){
+//         console.log(i);
+//     }
+// }
+
+for( var i = 4; i > -4; i-=1.5){
+    console.log(i);
+}

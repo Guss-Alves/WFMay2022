e7c7f7ebddeba8c75ef26442a53a3f0ae79b5a94
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2022-06-06 17:39:02 -0700

    hello world assignment

---
+++ Python-WORK/fundamentals/intro/intro.py
@@ -0,0 +1,26 @@
+# print('hello wolrd')
+
+# students_name = ['Gustavo', 'Luna', 'Teo']
+
+# for students in students_name:
+#     print(students)
+
+import random
+
+print('Welcome to Python!')
+
+print('This is a loop printing 5 times')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif(day == 'Friday'):
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')

4ce146771352ae122526433cd0b21e60d125be72
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2022-07-12 13:55:30 -0700

    Axios pokemon

--- MERN-WORK/API/pokemon-api/src/App.js
+++ MERN-WORK/API/pokemon-api/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import FetchPokemon from "./components/FetchPokemon";
+import AxiosPokemon from "./components/AxiosPokemon";
 
 function App() {
     return (
         <div className="App">
-            <FetchPokemon></FetchPokemon>
+            {/* <FetchPokemon></FetchPokemon> */}
+            <AxiosPokemon></AxiosPokemon>
         </div>
     );
 }

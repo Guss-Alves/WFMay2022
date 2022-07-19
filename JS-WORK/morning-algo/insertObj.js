f7907c615284449cff48e43bf35ad06f37493dd7
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2022-07-19 12:00:03 -0700

    algo

---
+++ JS-WORK/morning-algo/insertObj.js
@@ -0,0 +1,33 @@
+/* 
+  Given a table name string and an object whose key value pairs represent column names and values for the columns
+  return a SQL insert statement string
+  Tip: string interpolation (using back ticks, the key to the left of num 1 key) make it easy to add variables into a string or to add quotations without needing to escape them.
+  Bonus: after solving it, write a 2nd solution focusing on functional programming using built in methods
+*/
+
+const table = "users";
+const insertData1 = { first_name: "John", last_name: "Doe" };
+const expected1 =
+    "INSERT INTO users (first_name, last_name) VALUES ('John', 'Doe');";
+
+// Bonus:
+const insertData2 = {
+    first_name: "John",
+    last_name: "Doe",
+    age: 30,
+    is_admin: false,
+};
+const expected2 =
+    "INSERT INTO users (first_name, last_name, age, is_admin) VALUES ('John', 'Doe', 30, false);";
+// Explanation: no quotes around the int or the bool, technically in SQL the bool would become a 0 or 1, but don't worry about that here.
+
+function insert(obj){
+    let keys = [];
+    let values = [];
+    for(let property in obj){
+        keys.push(property)
+        values.push(obj[property])
+    }
+    return `INSERT INTO users (${keys}) VALUES (${values})`
+}
+console.log(insert(insertData2))

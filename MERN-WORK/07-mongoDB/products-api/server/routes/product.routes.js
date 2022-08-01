278ce62ab4faa46f9225344a4c8c38baac02c345
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2022-08-01 10:32:01 -0700

    changes

--- MERN-WORK/07-mongoDB/products-api/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-
-module.exports = (app)=>{
-    app.get('/api/products', ProductController.showAllProducts),
-    app.get('/api/product/:id', ProductController.showOne),
-    app.post('/api/products/new', ProductController.createNewProduct),
-    app.put('/api/product/edit/:id', ProductController.updateProduct),
-    app.delete('/api/product/delete/:id', ProductController.deleteProduct)
-}

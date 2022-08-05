797d9fd632576f2b26757def9b1226975b95a129
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2022-08-05 14:40:34 -0700

    daikichi assignment

--- JS-WORK/morning-algo/SLL2.js
+++ JS-WORK/morning-algo/SLL2.js
@@ -1,5 +1,5 @@
 class ListNode {
-    constructor( data ) {
+    constructor(data) {
         this.data = data;
         this.next = null;
     }
@@ -11,14 +11,14 @@ class SinglyLinkedList {
     }
 
     isEmpty() {
-        if ( this.head ) {
+        if (this.head) {
             return false;
         }
         return true;
     }
-    
-    insertAtFront( data ) {
-        let newNode = new ListNode( data );
+
+    insertAtFront(data) {
+        let newNode = new ListNode(data);
         newNode.next = this.head;
         this.head = newNode;
         return;
@@ -26,19 +26,19 @@ class SinglyLinkedList {
 
 
     removeFromFront() {
-        if( !this.isEmpty){
+        if (!this.isEmpty) {
             this.head = this.head.next;
         }
         return;
     }
 
-    insertAtBack( data ) {
-        let newNode = new ListNode( data );
-        if ( this.isEmpty() ){
+    insertAtBack(data) {
+        let newNode = new ListNode(data);
+        if (this.isEmpty()) {
             this.head = newNode;
         } else {
             let runner = this.head;
-            while( runner.next ){
+            while (runner.next) {
                 runner = runner.next;
             }
             runner.next = newNode;
@@ -46,76 +46,116 @@ class SinglyLinkedList {
         return;
     }
 
-    display(){
-        if( this.isEmpty() ){
+    display() {
+        if (this.isEmpty()) {
             console.log("This list is empty!")
         } else {
             let n = 0;
             let runner = this.head;
-            console.log('node ' + n + ': '+runner.data);
-            while( runner.next ){
+            console.log('node ' + n + ': ' + runner.data);
+            while (runner.next) {
                 runner = runner.next;
                 n++;
-                console.log('node ' + n + ': '+runner.data);
+                console.log('node ' + n + ': ' + runner.data);
             }
         }
         return;
     }
-/**
-   * Calls insertAtBack on each item of the given array.
-   * - Time: O(n * m) n = list length, m = arr.length.
-   * - Space: O(1) constant.
-   * @param {Array<any>} vals The data for each new node.
-   * @returns {SinglyLinkedList} This list.
-   */
-    seedFromArr(vals) {}
+    /**
+       * Calls insertAtBack on each item of the given array.
+       * - Time: O(n * m) n = list length, m = arr.length.
+       * - Space: O(1) constant.
+       * @param {Array<any>} vals The data for each new node.
+       * @returns {SinglyLinkedList} This list.
+       */
+    seedFromArr(values) {
+        this.head = null;
+        for (let value of values) {
+            this.insertAtBack(value);
+        }
+    }
 
- /**
-     * Converts this list into an array containing the data of each node.
-     * - Time: O(n) linear.
-     * - Space: O(n).
-     * @returns {Array<any>} An array of each node's data.
-     */
-    toArr() {}
- 
- /**
-    * Creates a new node with the given data and inserts it at the back of
-    * this list.
-    * - Time: O(?).
-    * - Space: O(?).
-    * @param {any} data The data to be added to the new node.
-    * @param {?ListNode} runner The current node during the traversal of this list
-    *    or null when the end of the list has been reached.
-    * @returns {SinglyLinkedList} This list.
-    */
+    /**
+         * Converts this list into an array containing the data of each node.
+         * - Time: O(n) linear.
+         * - Space: O(n).
+         * @returns {Array<any>} An array of each node's data.
+         */
+    toArr() {
+        var newArr = [];
+        let runner = this.head;
+        while (runner) {
+            newArr.push(runner.value);
+            runner = runner.next;
+        }
+        return newArr;
+    }
+    /**
+        * Creates a new node with the given data and inserts it at the back of
+        * this list.
+        * - Time: O(?).
+        * - Space: O(?).
+        * @param {any} data The data to be added to the new node.
+        * @param {?ListNode} runner The current node during the traversal of this list
+        *    or null when the end of the list has been reached.
+        * @returns {SinglyLinkedList} This list.
+        */
     insertAtBackRecursive(data, runner = this.head) {
-        if(runner.next == null){
+        if (runner.next == null) {
             let lastcar = new ListNode(data)
             runner.next = lastcar;
             return lastcar
         }
     }
+    /**
+ * Removes the last node of this list.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @returns {any} The data from the node that was removed.
+ */
+    removeBack() {
+        if (this.isEmpty()) {
+            return null
+        }
+        while (node.next.next) {
+            node = node.next;
+        }
+        let lastNode = node.next;
+        node.next = null;
+        return lastNode;
 
-
+    }
+    /**
+ * Determines whether or not the given search value exists in this list.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} val The data to search for in the nodes of this list.
+ * @returns {boolean}
+ */
+    contains(value) {
+        let node = this.head;
+        while (node != null && node.data != value) {
+            node = node.next;
+        }
+        if (node != null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
 let list1 = new SinglyLinkedList();
-list1.removeFromFront();
-console.log(list1.isEmpty());
-list1.insertAtBack("all");
-list1.insertAtBack("my");
-list1.insertAtBack("life");
-list1.insertAtBack("I");
-list1.insertAtBack("was");
-list1.insertAtBack("waiting");
-console.log(list1.isEmpty());
+// list1.removeFromFront();
+// console.log(list1.isEmpty());
+list1.insertAtBack(3);
+list1.insertAtBack(2);
 
-list1.removeFromFront();
-list1.removeFromFront();
-list1.removeFromFront();
-list1.insertAtFront("grumpy")
-list1.insertAtFront("bumpy")
-list1.insertAtFront("mklumpy")
-list1.insertAtFront("dumpy")
+// console.log(list1.isEmpty());
+
+list1.insertAtBack(1);
+list1.insertAtBack(4);
+console.log(list1.contains(5))
 
 list1.display();

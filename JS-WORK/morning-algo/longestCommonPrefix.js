4f485e5ea40f9ecd87b3716c08d70b6be77cf65b
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-01-17 15:43:36 -0800

    algo

---
+++ JS-WORK/morning-algo/longestCommonPrefix.js
@@ -0,0 +1,27 @@
+
+array = ["flower", "flow", "flight"]
+output = "fl";
+
+array2 = ["flower", "dog"]
+//or empty string = "";
+
+//GO OVER TO UNDERSTAND WHATS HAPPPENING HERE
+const commonPrefix = (strs) => {
+    let prefix = "";
+    if (strs.length == 0){
+        return prefix
+    } 
+    //loop through characters
+    for (let i = 0; i < strs[0].length; i++) {
+        const character = strs[0][i];
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i] != character){
+                return prefix
+            } 
+        }
+        prefix = prefix + character;
+    }
+    return prefix;
+}
+
+console.log(commonPrefix(array))

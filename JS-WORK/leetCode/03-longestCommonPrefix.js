c279901a3a2531f1537372f5ef0deb7957b61169
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-03-27 14:47:32 -0700

    common prefix

--- JS-WORK/leetCode/03-longestCommonPrefix.js
+++ JS-WORK/leetCode/03-longestCommonPrefix.js
@@ -1,17 +1,26 @@
 
 
-var longestCommonPrefix = function (strs) {
+const longestCommonPrefix = (word)=> {
+    let prefix = '';
+    //if array is empty we can already return nothing;
+    if(word.length === 0){
+        return prefix;
+    }
 
-    let prefix = strs[0];
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
+    //loop through characters
+    for(let i = 0; i < word[0].length; i++){
+        const character = word[0][i]
+        // console.log(character)
+        for(let j = 1; j < word.length; j++){
+            if( word[j][i] !== character ){
+                return prefix
+            }
         }
+        prefix = prefix + character;
     }
     return prefix;
 };
+
 array = ["flower","flow","flight"]
 
 console.log(longestCommonPrefix(array));
-
-// level easy of algo

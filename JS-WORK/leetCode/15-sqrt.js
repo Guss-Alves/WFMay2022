601d61055eae5288c00586fa89745c6464495162
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-04-17 18:32:52 -0700

    algo

---
+++ JS-WORK/leetCode/15-sqrt.js
@@ -0,0 +1,11 @@
+let x1 = 4;
+let x2 = 8;
+
+
+var mySqrt = function(x) {
+    let squareRoot = Math.sqrt(x);
+    let result = Math.floor(squareRoot);
+    return result;
+};
+
+console.log(mySqrt(x2));

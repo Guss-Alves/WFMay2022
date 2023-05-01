4f971bd37080a042ab22d4b061abc079c9cb580a
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-05-01 16:53:49 -0700

    algo

--- JS-WORK/leetCode/03-longestCommonPrefix.js
+++ JS-WORK/leetCode/03-longestCommonPrefix.js
@@ -10,12 +10,14 @@ const longestCommonPrefix = (word)=> {
     //loop through characters
     for(let i = 0; i < word[0].length; i++){
         const character = word[0][i]
-        // console.log(character)
+        // console.log(word.length);
+        //Now we loop through the other words, and check if its different from charaters of the word at index 0
         for(let j = 1; j < word.length; j++){
             if( word[j][i] !== character ){
                 return prefix
             }
         }
+        //If we get here it means the characters match, so we add to the variable 'prefix'
         prefix = prefix + character;
     }
     return prefix;

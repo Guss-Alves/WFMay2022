047efbf76c7fd1c50b68ad129f050795eefb7b22
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-09-04 20:46:17 -0700

    algo to refresh memory

--- JS-WORK/leetCode3/50-flippingImage.js
+++ JS-WORK/leetCode3/50-flippingImage.js
@@ -37,4 +37,43 @@ var flipAndInvertImage = function(image) {
     }
     return image;
 };
-console.log(flipAndInvertImage(image2));
+// console.log(flipAndInvertImage(image2));
+
+
+/* 
+Example 1:
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+                [0,1,1],[1,0,1],[0,0,0] reverse row
+                [1,0,0],[0,1,0],[1,1,1] invert img
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+Example 2:
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+*/
+//I AM DOING OVER AGAIN JUST TO WARM UP AFTER THE TRIP
+
+let image11 = [[1,1,0],[1,0,1],[0,0,0]]; //[[1,0,0],[0,1,0],[1,1,1]]
+let image22 = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]] //[[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+//fAndR function
+
+const fAndR = (img) =>{
+    console.log(img);
+    for(let i = 0; i < img.length; i++){
+        img[i].reverse();
+        for(let j = 0; j < img[i].length; j++){
+            if(img[i][j] == 0){
+                img[i][j] = 1
+            }else{
+                img[i][j] = 0
+            }
+        }
+    }
+    return img;
+}
+console.log(fAndR(image11));

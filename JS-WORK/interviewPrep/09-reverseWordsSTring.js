9969ba92160f93b241c42b9bfe3086ff8b5e773c
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-09-26 13:35:03 -0700

    more of algo

--- JS-WORK/interviewPrep/09-reverseWordsSTring.js
+++ JS-WORK/interviewPrep/09-reverseWordsSTring.js
@@ -1,19 +1,35 @@
 /* brainstorm 
-
+    Had to take a deeper look to understand how all the methods are working in the algo
 */
 
 let s1 = "the sky is blue" //"blue is sky the"
 let s2 = "  hello world  " // "world hello"
 let s3 = "a good   example" //Output: "example good a"
 
+//My version doing slowly and understanding whats happening on each line
 var reverseWords = function(s) {
     // Split the string into words
     let words = s.split(' ');
-    // Filter out any empty words
-    let nonEmptyWords = words.filter(word => word.trim() !== '');
-      // Reverse the array of non-empty words
-    let reversedWords = nonEmptyWords.reverse();
-
-    return reversedWords.join(' ');
+    // // Filter out any empty words
+    let realWords = words.filter(word => word.trim() !== '');
+    //   // Reverse the array of non-empty words
+    let reverseWords = realWords.reverse();
+    //join both string with a space between
+    reverseWords = reverseWords.join(' ');
+    return reverseWords;
+    
 };
-console.log(reverseWords(s3))
+console.log(reverseWords(s2))
+
+
+//version with IA
+// var reverseWords = function(s) {
+//     // Split the string into words
+//     let words = s.split(' ');
+//     // Filter out any empty words
+//     let nonEmptyWords = words.filter(word => word.trim() !== '');
+//       // Reverse the array of non-empty words
+//     let reversedWords = nonEmptyWords.reverse();
+
+//     return reversedWords.join(' ');
+// };

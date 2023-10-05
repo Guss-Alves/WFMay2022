3832122d49bb30917a5fbd18c7f0c76e37a7ceb9
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-10-05 13:54:42 -0700

    algo of the day

---
+++ JS-WORK/interviewPrep/12-evenOdd.js
@@ -0,0 +1,9 @@
+// the goal is to decide if the number is odd or even
+
+for(let i = 0; i < 100; i++){
+    if(i % 2 == 0){
+        console.log(i,"-> even")
+    }else{
+    console.log(i, "-> odd")
+    }
+}

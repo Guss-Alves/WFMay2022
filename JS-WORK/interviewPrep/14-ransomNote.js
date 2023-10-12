062df6b2e3cd82396e4482443fb023f93a24682c
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-10-12 13:23:40 -0700

    algo

--- JS-WORK/interviewPrep/14-ransomNote.js
+++ JS-WORK/interviewPrep/14-ransomNote.js
@@ -5,14 +5,29 @@ let ransomNote1 = "a"; let magazine1 = "b"; //Output: false
 let ransomNote2 = "aa"; let magazine2 = "ab"; //Output: false
 
 //Example 3:
-let ransomNote3 = "aa"; let magazine3 = "aab"; //Output: true
+let ransomNote3 = "abcabcdd"; let magazine3 = "ddcbacba"; //Output: true
 
-var canConstruct = function (ransomNote, magazine) {
+//both ways work
+//this one uses different for loop
+var canConstruct1 = function (ransomNote, magazine) {
     for (const char of magazine) {
+        console.log(char);
         ransomNote = ransomNote.replace(char, "");
+        console.log(ransomNote);
     }
 
     if (!ransomNote) return true;
     else return false;
 };
-console.log(canConstruct(ransomNote1, magazine1));
+console.log(canConstruct1(ransomNote3, magazine3));
+
+//this one uses classic for loop
+var canConstruct2 = function (ransomNote, magazine) {
+    for(let i = 0; i < magazine.length; i++){
+        ransomNote = ransomNote.replace(magazine[i], "");
+    }
+
+    if (!ransomNote) return true;
+    else return false;
+};
+console.log(canConstruct2(ransomNote3, magazine3));

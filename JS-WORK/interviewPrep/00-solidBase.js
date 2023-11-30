72aacb6d3ff0da5faa84e7300648e8057c0e8920
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-11-30 10:42:53 -0800

    algo

--- JS-WORK/interviewPrep/00-solidBase.js
+++ JS-WORK/interviewPrep/00-solidBase.js
@@ -1,1 +1,28 @@
 //practicing soon...
+
+//challenges
+const test = ()=>{
+    //for loop till 100
+    for(let i = 0; i < 100; i++){
+        console.log("FOR", i);
+    }
+    //while
+    let b = 1
+    while(b < 5){
+        if(b < 5){
+            console.log("B HERE", b);
+            b++;
+        }
+    }
+}
+const test2 = ()=>{
+    let arr = [1,2,3,4,5];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 3 == 0 || arr[i] % 5 == 0){
+            console.log(arr[i]);
+        }
+    }
+}
+
+// console.log(test());
+console.log(test2());

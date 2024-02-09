e6b1a51e41e06ba81f770cf1fc4823a771eafd7e
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2024-02-08 16:06:26 -0800

    algo of the day

---
+++ JS-WORK/interviewPrep/15-solidBase.js
@@ -0,0 +1,10 @@
+
+let arr1 = [0,10,20,30,40,50];
+
+const reviewBase = (array)=>{
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i])
+    }
+}
+
+console.log(reviewBase(arr1));

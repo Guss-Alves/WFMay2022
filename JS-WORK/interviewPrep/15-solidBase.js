7957c3a4760477f3aefa9372746b34fbec8a94d5
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2024-02-10 10:37:08 -0800

    algo of the day

--- JS-WORK/interviewPrep/15-solidBase.js
+++ JS-WORK/interviewPrep/15-solidBase.js
@@ -1,5 +1,6 @@
 
 let arr1 = [0,10,20,30,40,50];
+let arr2 = [0,20,30,50,-1,-30,100,40,10];
 
 const reviewBase = (array)=>{
     for(let i = 0; i < array.length; i++){
@@ -7,4 +8,10 @@ const reviewBase = (array)=>{
     }
 }
 
-console.log(reviewBase(arr1));
+const sortArr = (arr)=>{
+    arr.sort((a, b) => a -b)
+    console.log(arr);
+}
+
+// console.log(reviewBase(arr1));
+console.log(sortArr(arr2));

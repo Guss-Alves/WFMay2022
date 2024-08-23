acb7595d8c8521226d13ad99617fdeca960aec0b
Gustavo Alves Magalhaes <104655523+Guss-Alves@users.noreply.github.com>
2024-08-22 20:35:08 -0700

    algo of the day

--- JS-WORK/a-interviewPrep/17-dayBack.js
+++ JS-WORK/a-interviewPrep/17-dayBack.js
@@ -15,5 +15,57 @@ function reverseString(str) {
     }
     return reversed.join("");
 }
+// console.log(reverseString("momo")); // Output: "olleh"
 
-console.log(reverseString("bebe")); // Output: "olleh"
+
+
+/*
+2. Sum of Array Elements
+Challenge:
+Write a function that takes an array of numbers as input and returns the sum of all the elements in the array.
+
+// Example:
+*/
+function sumArray(arr) {
+    // Your code here
+    //var to track the value
+    let totalsum = 0;
+    for(let i = 0; i < arr.length; i++){
+        totalsum += arr[i];
+    }
+    return totalsum;
+}
+
+// console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+
+/* 
+3. Check for Palindrome
+Challenge:
+Write a function that takes a string as input and returns true if the string is a palindrome, and false otherwise. A palindrome is a word, phrase, or sequence that reads the same backward as forward (ignoring spaces and punctuation).
+Example:
+*/
+/*
+i
+madam
+    j
+*/
+
+function isPalindrome(str) {
+    // Your code here
+    let j = str.length - 1;
+    let i = 0;
+    while(j >= 0){
+        if(str[j] == str[i]){
+            j--;
+            i++;
+        }
+        if(str[j] !== str[i]){
+            return false
+        }
+        
+    }
+    return true;
+}
+
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false

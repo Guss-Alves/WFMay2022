8ad60002314122b6591140b0cb1f6dee933ec76e
Gustavo Alves Magalhaes <104655523+Guss-Alves@users.noreply.github.com>
2024-09-25 06:15:36 -0700

    practice algo

--- JS-WORK/a-algoWeek1.js/easyLevel02.js
+++ JS-WORK/a-algoWeek1.js/easyLevel02.js
@@ -184,6 +184,3 @@ const mergeIntervals = (intervals) => {
 // Example calls:
 console.log(mergeIntervals([[1, 3], [2, 6], [8, 10], [15, 18]])); // Output: [[1, 6], [8, 10], [15, 18]]
 console.log(mergeIntervals([[1, 4], [4, 5]]));                    // Output: [[1, 5]]
-
-//practice on other easy-medium level algos
-// 1 algo tomorrow

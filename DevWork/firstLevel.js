58f2ac1554de636416550558579f21e07f5de993
Gustavo Alves Magalhaes <104655523+Guss-Alves@users.noreply.github.com>
2024-12-11 21:17:00 -0800

    algo

---
+++ DevWork/firstLevel.js
@@ -0,0 +1,17 @@
+
+let stock1= ["apple", 101];
+let stock2 = ["Tesla", 110];
+
+function whichStock(s1, s2){
+//create logic
+if(s1[1] > s2[1]){
+    return "Buy " + s1[0] + " now!!, good price at " + s1[1]
+}else{
+    return `Buy ${s2[0]} now!!, good price at ${s2[1]}`
+}
+
+}
+
+//test cases
+
+console.log(whichStock(stock1, stock2));

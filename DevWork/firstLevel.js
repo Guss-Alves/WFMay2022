ee097e1f5011860461f5f2207458939f0e8163c1
Gustavo Alves Magalhaes <104655523+Guss-Alves@users.noreply.github.com>
2025-01-21 20:59:14 -0800

    update

--- DevWork/firstLevel.js
+++ DevWork/firstLevel.js
@@ -5,7 +5,7 @@ let stock2 = ["Tesla", 110];
 function whichStock(s1, s2){
 //create logic
 if(s1[1] > s2[1]){
-    return "Buy " + s1[0] + " now!!, good price at " + s1[1]
+    return "Buy " + s1[0] + " now!!, check this out good price at " + s1[1]
 }else{
     return `Buy ${s2[0]} now!!, good price at ${s2[1]}`
 }
